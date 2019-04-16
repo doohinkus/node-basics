6c3a0beb471a2fd6c8594d3e9a7e69f3dd9ad3dc
doohinkus <doohinkus@gmail.com>
2019-04-16 10:29:34 -0700

    update comments

--- express.js
+++ express.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const app = express();
 const PORT = 3000;
+
+//ALWAYS SET ENV--Apps run 70% SLOWER IN DEV.
+//CMD -> NODE_ENV=production node app.js
+//Eg NODE_ENV=production node express.js
+//Eg NODE_ENV=production npm start
+
+//Whenever you use USE use next()!
+
+
 app.use((req,res,next) => {
     res.setHeader('x-server-date', new Date());
     return next();
@@ -20,6 +29,18 @@ app.get('/hello/:name', (req, res, next) => {
     return res.send(`Hello ${req.params.name}`)
 });
 
+app.get('/throw', (req,res, next) => {
+    throw new Error('Something got fucked up!!! Check below: ')
+});
+app.get('/next', (req,res, next) => {
+    setTimeout(()=>{
+        //Throw doesn't work alone. ALWAYS USE NEXT with errors.
+        // EG: throw new Error('Something got fucked up!!! Check below: '); 
+        //No error message app hangs
+        // This works!! Always use next with errors!!!
+        next(new Error('Something got fucked up!!! Check below: '));
+    });
+});
 app.listen(PORT, () => {
     console.log(`App listening on ${PORT}`)
 })
